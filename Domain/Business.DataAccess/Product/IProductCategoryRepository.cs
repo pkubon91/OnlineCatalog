b4980003c4c91248ef32b495f1c7f5e610f12e25
Piotr Kubon <Piotr.Kubon@sabre.com>
2016-09-15 19:38:26 +0200

    Add product category service and product category repository

---
+++ Domain/Business.DataAccess/Product/IProductCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Business.Products;
+
+namespace Business.DataAccess.Product
+{
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
+    {
+        ProductCategory GetProductCategoryById(int id);
+
+        ProductCategory EditProductCategory(int productCategoryId, ProductCategory productCategory);
+
+        void RemoveProductCategory(int id);
+    }
+}
---
+++ Domain/Business.DataAccess/Product/ProductCategoryRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Business.NHibernate;
+using Business.Products;
+
+namespace Business.DataAccess.Product
+{
+    public class ProductCategoryRepository : IProductCategoryRepository
+    {
+        private readonly ISessionProvider _sessionProvider;
+
+        public ProductCategoryRepository(ISessionProvider sessionProvider)
+        {
+            if (sessionProvider == null) throw new ArgumentNullException(nameof(sessionProvider));
+            _sessionProvider = sessionProvider;
+        }
+
+        public void AddToDatabase(ProductCategory entity)
+        {
+            if(entity == null) throw new ArgumentNullException(nameof(entity));
+
+            using (var session = _sessionProvider.CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    session.Save(entity);
+                    transaction.Commit();
+                }
+            }
+        }
+
+        public ProductCategory GetProductCategoryById(int id)
+        {
+            using (var session = _sessionProvider.CreateSession())
+            {
+                return session.Get<ProductCategory>(id);
+            }
+        }
+
+        public ProductCategory EditProductCategory(int productCategoryId, ProductCategory productCategory)
+        {
+            if(productCategory == null) throw new ArgumentNullException(nameof(productCategory));
+            
+            using (var session = _sessionProvider.CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    session.Update(productCategory);
+                    transaction.Commit();
+                }
+            }
+            return productCategory;
+        }
+
+        public void RemoveProductCategory(int id)
+        {
+            var productCategory = GetProductCategoryById(id);
+            if (productCategory == null) return;
+
+            using (var session = _sessionProvider.CreateSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    session.Delete(productCategory);
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
